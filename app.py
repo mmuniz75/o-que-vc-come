7c91d2d000a5388add60cac0cd605a92d959c0c1
Marcelo <marcelo.muniz@platformbuilders.io>
2019-09-09 10:37:10 -0300

    remove debug

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ api = Api(app)
 api.add_resource(ChemicalResource, '/chemicals')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
