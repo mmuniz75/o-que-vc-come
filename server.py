6f89a95209ee8d9ecc0373c64b3bd344c388023c
mmuniz75 <mmunizs1975@gmail.com>
2020-04-12 12:04:32 -0300

    correcao de configuracao heroku

--- server.py
+++ server.py
@@ -4,18 +4,23 @@ from routes import app
 
 import os
 
-
 @app.before_first_request
 def create_db():
     db.create_all()
 
 
-db.init_app(app)
-
 if __name__ == '__main__':
+    db.init_app(app)
     if 'OQVC_DEVELOP' in os.environ:
         app.run(host='0.0.0.0', port=5000, debug=True)
-    ''''    
+    else:
+        app.run()
+
+    ''''
+    if 'OQVC_DEVELOP' in os.environ:
+        db.init_app(app)
+        app.run(host='0.0.0.0', port=5000, debug=True)
+        
     else:
         http_server = WSGIServer(('', 5000), app)
         http_server.serve_forever()
